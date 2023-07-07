f4206db4faca874560dcf21eab2144581956ed25
klmlboom1013 <root@LeeHS>
2023-07-07 10:50:01 +0900

    setting

---
+++ src/main/java/com/wpay/core/merchant/WpayCoreMerchantApplication.java
@@ -0,0 +1,13 @@
+package com.wpay.core.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpayCoreMerchantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpayCoreMerchantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wpay/core/merchant/WpayCoreMerchantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpay.core.merchant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpayCoreMerchantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
