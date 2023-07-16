f3c1047ee86f2a0a50a84e8793687a7ab399cc71
klmlboom1013 <klmlboom1013@gmail.com>
2023-07-16 13:25:41 +0900

    404 핸들러

--- src/main/java/com/wpay/core/merchant/WpayCoreMerchantApplication.java
+++ src/main/java/com/wpay/core/merchant/WpayCoreMerchantApplication.java
@@ -3,13 +3,21 @@ package com.wpay.core.merchant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @ServletComponentScan
-public class WpayCoreMerchantApplication {
+public class WpayCoreMerchantApplication extends SpringBootServletInitializer {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(WpayCoreMerchantApplication.class, args);
+//	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(WpayCoreMerchantApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(WpayCoreMerchantApplication.class, args);
+		DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
 	}
-
 }
