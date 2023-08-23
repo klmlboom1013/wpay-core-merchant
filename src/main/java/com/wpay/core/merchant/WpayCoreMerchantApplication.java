c9b9b863b841c4d1775461f16ead67492e323c85
klmlboom1013 <klmlboom1013@gmail.com>
2023-08-23 17:18:37 +0900

    설정 수정

--- src/main/java/com/wpay/core/merchant/WpayCoreMerchantApplication.java
+++ src/main/java/com/wpay/core/merchant/WpayCoreMerchantApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan(basePackages = {"com.wpay"})
 @SpringBootApplication
 @ServletComponentScan
+@EnableDiscoveryClient
 public class WpayCoreMerchantApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
